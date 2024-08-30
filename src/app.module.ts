a789546ea11575a77f55d503d20c523d9b8f8a1a
lc <486836332@qq.com>
2024-08-30 19:28:33 +0800

    feat: static

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,22 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { resolve } from 'node:path';
 import { APP_GUARD } from '@nestjs/core';
-import { AuthGuard } from 'src/auth/auth.guard';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
+import { LoggerMiddleware } from 'middleware/logger';
+import { AuthGuard } from 'src/auth/auth.guard';
+
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-import { LoggerMiddleware } from 'middleware/logger';
-
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [
+    ServeStaticModule.forRoot({ rootPath: resolve(process.cwd(), 'static') }),
+    UserModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [
     AppService,
